9f151df0c60a4c27984ac35b6e39c59e96905414
Muhammad Omer Ali Malik <omeralimalik96@gmail.com>
2022-02-03 20:21:02 +0500

    Testing non-card View for post component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             date="29 February 2020"
             url="https://www.youtube.com/watch?v=LMagNcngvcU&ab_channel=JavaScriptMastery"
             description="This is a game that uses quantum circuits and qauntum game theory to simulate pacman in an entangled state instance. Winners and losers are just predicted"
-            card={true}
+            card={false}
           />
       </div>
   );

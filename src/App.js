81b40f8dc7c11b65b714f1392c924bd9217fae16
Muhammad Omer Ali Malik <omeralimalik96@gmail.com>
2022-02-05 17:01:54 +0500

    In App.js the App component does not have a classname for its root div

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-      <div className="root">
+      <div className="">
           <Post 
             title="Quantum Pacman"
             author="Omer Ali Malik"
             date="29 February 2020"
             url="https://www.youtube.com/watch?v=LMagNcngvcU&ab_channel=JavaScriptMastery"
             description="This is a game that uses quantum circuits and qauntum game theory to simulate pacman in an entangled state instance. Winners and losers are just predicted"
-            card={true}
+            card={false}
           />
           {/* <Post 
             title="Quantum Pacman"

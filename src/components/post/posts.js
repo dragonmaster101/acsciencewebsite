703f6d564a6181765d1905bf3e62a3e00e1d5e1b
Muhammad Omer Ali Malik <omeralimalik96@gmail.com>
2022-02-07 02:54:13 +0500

    Revert "Search Bar is hooked up to the api and works completely;Loading icons shows when it is fetching results. There is a message for queries with nil results"
    This reverts commit f8cb56a930c786d13c2cfc0b3b1c0fb1c0b45d3f.

--- src/components/post/posts.js
+++ src/components/post/posts.js
@@ -1,4 +1,3 @@
-import ReactLoading from "react-loading";
 import {useState , useEffect} from "react"
 
 import { Post } from "./post"
@@ -36,63 +35,21 @@ function printPost(post) {
 
 export default function Posts(props){
     const [postsData , setPostsData] = useState(false);
-    const [query , setQuery] = useState(false);
-    let [searchForm , setSearchForm] = useState(false);
 
     useEffect(() => {
         getPosts("http://localhost:8080" , setPostsData);
     }, []);
 
-    searchForm = (
-        <FormControl
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
-                style={{padding: 10}}
-                placeholder="enter author/title and click the search button"
-                onChange={e => setQuery(e.target.value)}
-                />
-    );
-
     return (
         <div className="posts-Header">
             <h1 className="posts-Title">Projects</h1>
-            <InputGroup className="posts-Search" style={{width: "90%" , marginLeft: "4%" , marginBottom: "2%"}}>
+            <InputGroup className="mb-3">
                 <InputGroup.Text id="inputGroup-sizing-default"><BsSearch /></InputGroup.Text>
-                
-                {searchForm}
-
-
-                <Button variant="flat" onClick={() => {
-                    setPostsData(<ReactLoading 
-                        type={"spin"} color={"white"} height={'10%'} width={'10%'} />)
-
-                    fetch("http://localhost:8080/post/query/" + query , {
-                        method: 'GET',
-                        headers: {
-                            accept: 'application/json',
-                        },
-                    }).then(resp => {
-                        console.log(resp)
-                        if (resp.status === 669) {
-                            setPostsData(<h1 style={{color: "white"}}>No Matching Results</h1>)
-                            return;
-                        }
-                        resp.json().then(val => {
-                            let results = []
-                            for (let i=0;i < val.length;i++) {
-                                results.push((
-                                    <Col mg="auto">
-                                        <Post data={val[i]} card={true}/>
-                                    </Col>
-                                ))
-                            }
-                            setPostsData(results);
-                        })
-                    }).catch(err => alert(err))
-
-                }}>search</Button>
-
-
+                <FormControl
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                />
+                <Button variant="flat">search</Button>
             </InputGroup>
             <div className="posts-Body">
                 <Container fluid className="posts-Container">
